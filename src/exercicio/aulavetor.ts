0fa761f17fe2626115ac4ca717672a900fdf8d39
dantasfabyy1234-creator <dantasfabyy1234@gmail.com>
2026-04-16 15:53:10 -0300

    aula 16/04

---
+++ src/exercicio/aulavetor.ts
@@ -0,0 +1,18 @@
+//Metodos:
+let vetor:(number|string)[]=[10,200,"Maria", "jose"]
+
+vetor.push(30)  //adiciona elemento no final do vetor
+
+vetor.pop()   //remove elemento do final do vetor
+
+vetor.unshift("Roxo") //adiciona no inicio do vetor
+
+vetor.shift() //remove o primeiro
+
+vetor.reverse() //inverte o vetor
+
+vetor.sort() //ordena em ordem alfabetica e crescente
+
+let paleta = vetor.join("-") //adiciona algo no meio
+
+console.log (vetor)
---
+++ src/lista/condicional/questao1.ts
@@ -0,0 +1,11 @@
+// 1. Crie um programa que leia um número e informe se ele é:
+//  Par ou Ímpar
+//  Positivo ou Negativo
+let num=(Number(prompt("Informe o numero: ")))
+if(num>0){
+    document.writeln("O valor é positivo: ")
+}else{
+    document.writeln("O valor é negativo: ")
+}if(num%2==0){
+    document.writeln("O valor é par" )
+}else(" O valor é impar ")
