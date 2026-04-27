f909b4b8dc4cdcccb7e1235074aea68de7d59fa7
claraaoliveiraa01-ux <claraaoliveiraa01@gmail.com>
2026-04-27 19:47:27 -0300

    questao lista

---
+++ src/lista/arrays/questão1.ts
@@ -0,0 +1,23 @@
+// 1. Mostre-me as seguintes listas, derivadas de: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// a) Números pares
+// b) Números ímpares
+// c) Todos os múltiplos de 2,3 e 4
+// d) Lista reversa
+let vetor=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let pares =[]
+let impares= []
+let multi= []
+
+// correr o vetor
+for (let i=0; i<vetor.length -1; i++){
+    if (vetor [i]%2 ==0){
+        pares.push(vetor[i])
+    }else if (vetor[i]%2==1){
+        impares[i]= vetor[i]
+    }if (vetor[i]%2==0 && vetor[i]%3==0 && vetor[i]%4==0){
+        multi[i]=vetor[i]
+    }
+}
+console.log ("Pares: "+pares)
+console.log (" Impares: " + impares)
+console.log (" Multiplos: "+ multi)
--- src/lista/condicional/questao1.ts
+++ src/lista/condicional/questao1.ts
@@ -1,11 +1,11 @@
 // 1. Crie um programa que leia um número e informe se ele é:
 //  Par ou Ímpar
 //  Positivo ou Negativo
-let num=(Number(prompt("Informe o numero: ")))
+let num:number=(Number(prompt("Informe o numero: ")))
 if(num>0){
-    document.writeln("O valor é positivo: ")
+    console.log("O valor é positivo: ")
 }else{
-    document.writeln("O valor é negativo: ")
+    console.log("O valor é negativo: ")
 }if(num%2==0){
-    document.writeln("O valor é par" )
+    console.log("O valor é par" )
 }else(" O valor é impar ")
