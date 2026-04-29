a75427fb8705f439dfc88f1b83ff0f7c31ae2282
claraaoliveiraa01-ux <claraaoliveiraa01@gmail.com>
2026-04-29 00:30:46 -0300

    lista

--- src/lista/arrays/questão1.ts
+++ src/lista/arrays/questão1.ts
@@ -1,13 +1,8 @@
-// 1. Mostre-me as seguintes listas, derivadas de: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-// a) Números pares
-// b) Números ímpares
-// c) Todos os múltiplos de 2,3 e 4
-// d) Lista reversa
 let vetor=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 let pares =[]
 let impares= []
 let multi= []
-
+let reversa=[]
 // correr o vetor
 for (let i=0; i<vetor.length -1; i++){
     if (vetor [i]%2 ==0){
@@ -17,7 +12,9 @@ for (let i=0; i<vetor.length -1; i++){
     }if (vetor[i]%2==0 && vetor[i]%3==0 && vetor[i]%4==0){
         multi.push(vetor[i])
     }
+  reversa.unshift(vetor[i])
 }
 console.log ("Pares: "+pares)
 console.log (" Impares: " + impares)
 console.log (" Multiplos: "+ multi)
+console.log(reversa)
